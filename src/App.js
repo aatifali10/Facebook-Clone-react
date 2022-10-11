19811925ee122fcf12a85000888154bc4b2677e0
Aatif Ali <aatifwarraich10@gmail.com>
2022-10-11 23:18:22 +0500

    Left side now complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import "./index.css";
 import Header from "./component/header/Header";
+import LeftSideBar from "./component/leftside/LeftSideBar";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <div className="App">
+        <Header />
+      </div>
+      <div className="app-body">
+        <LeftSideBar />
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/component/leftside/LeftSideBar.js
+++ src/component/leftside/LeftSideBar.js
@@ -1,8 +1,28 @@
+import { Avatar } from "@mui/material";
 import React from "react";
-import "./leftsidebar.js";
+import "./leftsidebar.css";
+import Friends from "../../images/Friends.png";
+import groups from "../../images/groups.png";
+import watch from "../../images/watch.png";
+import Events from "../../images/Event.webp";
+import SiderbarOption from "./SiderbarOption";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 const LeftSideBar = () => {
-  return <div>LeftSideBar</div>;
+  return (
+    <div className="sidebar">
+      <SiderbarOption src={Avatar} title="Aatif Ali" />
+      <SiderbarOption
+        src="https://www.facebook.com/rsrc.php/v3/yR/r/tInzwsw2pVX.png"
+        title="Covid 19 Information center"
+      />
+      <SiderbarOption src={Friends} title="Friends" />
+      <SiderbarOption src={groups} title="Groups" />
+      <SiderbarOption src={Events} title="Watch" />
+      <SiderbarOption src={watch} title="Events" />
+      <SiderbarOption Icon={ExpandMoreIcon} title="See More" />
+    </div>
+  );
 };
 
 export default LeftSideBar;
